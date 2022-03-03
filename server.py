48d813adf8acfbd2dff319110aaa8eee71e22731
Lulky <lukyluxiano.12@gmail.com>
2022-03-03 13:44:37 -0500

    enrutamiento

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return "Hola Mundo"
+
+@app.route('/dojo')
+def dojo():
+    return "!Dojo!"
+
+@app.route('/say/<string:nombre>')
+def llamar(nombre):
+    return f"Hola, {nombre}!"
+
+@app.route('/repeat/<int:num>/<string:name>')
+def repeet(num, name):
+        return name * num
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
